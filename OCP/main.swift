27be100096aa8e2add474d2f5e6c6f525ce7e783
Andrew <andymuncey@users.noreply.github.com>
2026-03-04 13:51:21 +0000

    remove comments, adopt protocol instead of inherting class

--- OCP/main.swift
+++ OCP/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  OCP
-//
-//  Created by Andrew Muncey on 08/06/2015.
-//  Copyright (c) 2015 University of Chester. All rights reserved.
-//
-
 import Foundation
 
 //nothing to run - explore the project
--- Rectangle.swift
+++ Rectangle.swift
@@ -1,11 +1,3 @@
-//
-//  Rectangle.swift
-//  OCP
-//
-//  Created by Andrew Muncey on 08/06/2015.
-//  Copyright (c) 2015 University of Chester. All rights reserved.
-//
-
 import Foundation
 
 class Rectangle:Shape {
